ace2feae597a27df1a726b2765fa933627bbb0de
Felipe Estival <felipemauroestival@gmail.com>
2026-08-03 12:03:18 -0300

    views em andamento e mensagens de erro implementadas

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -13,8 +13,8 @@ def cadastro(request):
     Usuario = Person()
 
     if request.method == "POST":
-        name_digitado = request.POST.get("name")
-        email_digitado = request.POST.get("email")
+        name_digitado = request.POST.get("name").strip()
+        email_digitado = request.POST.get("email").strip().lower()
         password_digitado = request.POST.get("password")
         age_digitado = request.POST.get("age")
 
@@ -25,10 +25,33 @@ def cadastro(request):
             "age": age_digitado
         } 
 
-        Usuario = Person(
-            name = name_digitado, email = email_digitado, password = password_digitado, age = age_digitado
-        )
-        Usuario.save()
+        if len(name_digitado) < 3:
+            messages.error(request, "Insira um nome válido.")
+            return render(request, "cadastro.html", context)
+                
+        if not name_digitado.replace(" ", "").isalpha():
+            messages.error(request, "Insira um nome válido.")
+            return render(request, "cadastro.html", context)
+                
+        if not age_digitado.isdigit():
+            messages.error(request, "Insira uma idade válida.")
+            return render(request, "cadastro.html", context)
+        
+        age_num = int(age_digitado)
+        if age_num < 18 or age_num > 120:
+            messages.error(request, "Insira uma idade válida.")
+            return render(request, "cadastro.html", context)
+
+        try:
+            Usuario = Person(
+                name = name_digitado, email = email_digitado, password = password_digitado, age = age_digitado
+            )
+            Usuario.save()
+        except: 
+            messages.error(request, "Erro, não foi possível cadastrar.")
+            return render(request, "cadastro.html", context)
+
+        messages.success(request, "Cadastrado com sucesso!")
 
         return render(request, 'cadastro.html', context)
 
