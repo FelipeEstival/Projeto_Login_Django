aa222b2ee1696fc00f9630e57df7c4235a981cbb
Felipe Estival <felipemauroestival@gmail.com>
2026-08-03 11:28:27 -0300

    views e templates definidos

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,4 +3,6 @@ from myapp import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('login/', views.login, name="login"),
+    path('cadastro/', views.cadastro, name="cadastro"),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def cadastro(request):
+    if request.method == 200:
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        age = request.POST.get("age")
+
+    return render(request, 'cadastro.html')
